168e2798aeac8cd246316b9cbff06c7e277ebe4b
jannatnury <jannatnury253@gmail.com>
2022-05-20 23:11:20 +0600

    mongo user created

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express =require('express');
-const cors =require('cors');
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
 const app = express();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
+/**
+ * --------------------------------------------------
+ * Middleware
+ * --------------------------------------------------
+ */
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Sentholics server is running');
-});
-
-app.listen(port,()=>{
-    console.log("sentholics is running");
-});
