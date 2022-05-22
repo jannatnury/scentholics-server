fcb60d516621d6f4e21dadd354e5b4548468d953
jannatnury <jannatnury253@gmail.com>
2022-05-22 20:04:16 +0600

    db is connected with client side

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
-require('dotenv').config();
+// const ObjectId = require('mongodb').ObjectId;
+// require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,6 +15,61 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://user-nury:<password>@cluster0.6t6ti.mongodb.net/?retryWrites=true&w=majority";
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
+
+
+/**
+ * --------------------------------------------------
+ * Config database
+ * --------------------------------------------------
+ * Mongo DB
+ * --------------------------------------------------
+ */
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6t6ti.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("scentholics").collection("perfumes");
+
+//   // perform actions on the collection object
+//   console.log("Mongo is connected");
+//   client.close();
+// });
+
+
+
+/**
+ * --------------------------------------------------
+ * api
+ * --------------------------------------------------
+ */
+
+// async function products() {
+//     try {
+//         /**
+//          * --------------------------------------------------
+//          * Connect to MongoDB
+//          * --------------------------------------------------
+//          */
+//         await client.connect();
+//         const productCollection = client.db('Scentholics').collection('Perfumes');
+
+
+
+// root route
+
+app.get('/', (req, res) => {
+    res.send('scentholics server is running...');
+});
+
+     
+
+
+
+
+//  server listening
+
+app.listen(port, () => {
+    console.log(`Server started on port... ${port}`);
+});
